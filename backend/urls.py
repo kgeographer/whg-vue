5178ce09a419d46507473ea8d111ae89e517e633
Karl Grossner <karl.geog@gmail.com>
2018-09-01 13:20:22 -0600

    api basics stubbed in

--- backend/urls.py
+++ backend/urls.py
@@ -3,8 +3,8 @@ from backend import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'places/$', views.place_list),
-    url(r'places/(?P<pk>[0-9]+)$', views.place_detail),
+    url(r'places/$', views.PlaceList.as_view()),
+    url(r'places/(?P<pk>[0-9]+)$', views.PlaceDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/views.py
+++ backend/views.py
@@ -1,30 +1,11 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
 from backend.models import Place
 from backend.serializers import PlaceSerializer
+from rest_framework import generics
 
-@api_view(['GET'])
-def place_list(request, format=None):
-    """
-    List places
-    """
-    if request.method == 'GET':
-        places = Place.objects.all()[:20]
-        serializer = PlaceSerializer(places, many=True)
-        return Response(serializer.data)
+class PlaceList(generics.ListAPIView):
+    queryset = Place.objects.all()[:50]
+    serializer_class = PlaceSerializer
 
-@api_view(['GET'])
-def place_detail(request, pk, format=None):
-    """
-    Retrieve a place
-    """
-    try:
-        place = Place.objects.get(pk=pk)
-    except Place.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = PlaceSerializer(place)
-        return Response(serializer.data)
+class PlaceDetail(generics.RetrieveAPIView):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
